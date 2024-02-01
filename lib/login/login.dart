94d3212e99d119f34578264fa5fedb0e5ce99cdf
王鹏鹏 <874934733@qq.com>
2024-02-01 11:54:30 +0800

    1.

---
+++ lib/login/login.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import 'package:nurse/utils/HexToColor.dart';
+
+import '../utils/createmyinput.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({super.key});
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  void _login() {
+    // 在这里执行登录逻辑
+    // Navigator.pushReplacementNamed(context, '/home');
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+          child: Center(
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/icon_login_bg.png",
+              width: double.infinity,
+              fit: BoxFit.fill, // 完全填充
+              height: 260,
+            ),
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 26, bottom: 20),
+                  child: Text(
+                    "智慧养老管理系统",
+                    style: TextStyle(
+                        color: HexToColor("#4CA8FE"),
+                        fontSize: 26,
+                        fontWeight: FontWeight.bold),
+                  ),
+                )
+              ],
+            ),
+            CreateMyInput(
+                iconString: "assets/icon_acount.png",
+                placeholder: "请输入手机号",
+                isPassword: false,
+                inputController: _usernameController),
+            Container(
+              margin: const EdgeInsets.only(bottom: 50, top: 15),
+              child: CreateMyInput(
+                  iconString: "assets/icon_pwd.png",
+                  placeholder: "请输入密码",
+                  isPassword: true,
+                  inputController: _passwordController),
+            ),
+            GestureDetector(
+              child: Container(
+                width: double.infinity,
+                margin: const EdgeInsets.only(left: 42, right: 42),
+                height: 48,
+                decoration: BoxDecoration(
+                    gradient: const LinearGradient(colors: [
+                      Color.fromRGBO(85, 186, 254, 1),
+                      Color.fromRGBO(48, 114, 255, 1),
+                    ], begin: Alignment.topLeft, end: Alignment.bottomRight),
+                    borderRadius: BorderRadius.circular(24)),
+                child: const Center(
+                  child: Text(
+                    "登录",
+                    style: TextStyle(color: Colors.white, fontSize: 17),
+                  ),
+                ),
+              ),
+              onTap: () {},
+            )
+          ],
+        ),
+      )),
+    );
+  }
+}
---
+++ lib/utils/createmyinput.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:nurse/utils/HexToColor.dart';
+
+class CreateMyInput extends StatelessWidget {
+  final iconString;
+  final placeholder;
+  final isPassword;
+  final inputController;
+
+  const CreateMyInput(
+      {super.key,
+      this.iconString,
+      this.placeholder,
+      this.isPassword,
+      this.inputController});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: HexToColor("#F1F7FF"),
+          borderRadius: BorderRadius.circular(24),
+          border: Border.all(color: HexToColor("#83B5FF"), width: 1)),
+      margin: const EdgeInsets.only(left: 43, right: 43),
+      padding: const EdgeInsets.only(left: 20, right: 20),
+      width: double.infinity,
+      height: 48,
+      child: Row(
+        children: <Widget>[
+          Image.asset(iconString, width: 15, height: 15),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.only(left: 12),
+              child: TextField(
+                controller: inputController,
+                decoration: InputDecoration.collapsed(hintText: placeholder),
+                obscureText: isPassword, //是否是以星号*显示密码
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
